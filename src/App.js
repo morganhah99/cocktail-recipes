817453d41389c951fe834f25a7e77f168f9c98bc
morganhah <morganhah99@gmail.com>
2022-11-29 17:29:49 -0500

    set up api to fetch information about bloody mary ex: ingredients and instructions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  const API_KEY = 'LQfG4o46WCky/bl4BnGjrg==h5GhOQ54nyPvc0O1';
+
+  const fetchCocktailRecipe = async () => {
+    try {
+      const resp = await axios('https://api.api-ninjas.com/v1/cocktail?name=bloody mary', {
+        headers: {
+          'X-Api-Key': API_KEY,
+        },
+      });
+      console.log(resp)
+    } catch(error) {
+
+    }
+  }
+  fetchCocktailRecipe();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    hello
     </div>
   );
+  
 }
 
 export default App;
